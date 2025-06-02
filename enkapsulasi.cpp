1ccf6b5e74be24e0a57782cf9bb71a8335aa5bc3
Kipril20 <rafky2004@gmail.com>
2025-06-02 18:59:08 +0700

    memberikan value

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -20,3 +20,8 @@ public:
 int main()
 {
     remoteLampu lampuRumah;
+
+    lampuRumah.setSaklarNo(0, "Lampu Teraas Rumah");
+    lampuRumah.setSaklarNo(1, "Lampu Ruang Tamu");
+    lampuRumah.setSaklarNo(2, "Lampu Kamar Tidur");
+    lampuRumah.setSaklarNo(3, "Lampu Dapur");
